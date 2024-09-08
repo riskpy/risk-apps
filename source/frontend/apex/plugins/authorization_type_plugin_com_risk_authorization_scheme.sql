71d862ffa870f9dac46e514b91d8e579555cbfa4
Javier Meza <javier.meza.py@gmail.com>
2024-09-08 12:12:56 -0400

    Actualizar Risk Admin

--- source/frontend/apex/plugins/authorization_type_plugin_com_risk_authorization_scheme.sql
+++ source/frontend/apex/plugins/authorization_type_plugin_com_risk_authorization_scheme.sql
@@ -3,21 +3,26 @@ set define off verify off feedback off
 whenever sqlerror exit sql.sqlcode rollback
 --------------------------------------------------------------------------------
 --
--- ORACLE Application Express (APEX) export file
+-- Oracle APEX export file
 --
--- You should run the script connected to SQL*Plus as the Oracle user
--- APEX_200200 or as the owner (parsing schema) of the application.
+-- You should run this script using a SQL client connected to the database as
+-- the owner (parsing schema) of the application or as a database user with the
+-- APEX_ADMINISTRATOR_ROLE role.
+--
+-- This export file has been automatically generated. Modifying this file is not
+-- supported by Oracle and can lead to unexpected application and/or instance
+-- behavior now or in the future.
 --
 -- NOTE: Calls to apex_application_install override the defaults below.
 --
 --------------------------------------------------------------------------------
 begin
-wwv_flow_api.import_begin (
- p_version_yyyy_mm_dd=>'2020.10.01'
-,p_release=>'20.2.0.00.20'
-,p_default_workspace_id=>2600325421114677
+wwv_flow_imp.import_begin (
+ p_version_yyyy_mm_dd=>'2024.05.31'
+,p_release=>'24.1.1'
+,p_default_workspace_id=>16127146599720668
 ,p_default_application_id=>539
-,p_default_id_offset=>0
+,p_default_id_offset=>16129305178841323
 ,p_default_owner=>'RISK'
 );
 end;
@@ -28,30 +33,29 @@ prompt APPLICATION 539 - RISK ADMIN
 -- Application Export:
 --   Application:     539
 --   Name:            RISK ADMIN
---   Date and Time:   21:16 Tuesday June 1, 2021
---   Exported By:     JMEZA
+--   Date and Time:   16:07 Sunday September 8, 2024
+--   Exported By:     RISK
 --   Flashback:       0
 --   Export Type:     Component Export
 --   Manifest
---     PLUGIN: 115500696919685442
+--     PLUGIN: 131630002098526765
 --   Manifest End
---   Version:         20.2.0.00.20
---   Instance ID:     500134684140051
+--   Version:         24.1.1
+--   Instance ID:     9655952054092436
 --
 
 begin
   -- replace components
-  wwv_flow_api.g_mode := 'REPLACE';
+  wwv_flow_imp.g_mode := 'REPLACE';
 end;
 /
 prompt --application/shared_components/plugins/authorization_type/com_risk_authorization_scheme
 begin
-wwv_flow_api.create_plugin(
- p_id=>wwv_flow_api.id(115500696919685442)
+wwv_flow_imp_shared.create_plugin(
+ p_id=>wwv_flow_imp.id(131630002098526765)
 ,p_plugin_type=>'AUTHORIZATION TYPE'
 ,p_name=>'COM.RISK.AUTHORIZATION_SCHEME'
 ,p_display_name=>'RISK Authorization Scheme'
-,p_supported_ui_types=>'DESKTOP:JQM_SMARTPHONE'
 ,p_plsql_code=>wwv_flow_string.join(wwv_flow_t_varchar2(
 'FUNCTION is_authorized(p_authorization IN apex_plugin.t_authorization,',
 '                       p_plugin        IN apex_plugin.t_plugin)',
@@ -68,6 +72,7 @@ wwv_flow_api.create_plugin(
 ,p_api_version=>2
 ,p_execution_function=>'is_authorized'
 ,p_substitute_attributes=>true
+,p_version_scn=>39539363226869
 ,p_subscribe_plugin_settings=>true
 ,p_version_identifier=>'v0.1.0'
 ,p_about_url=>'https://jtsoya539.github.io/risk/'
@@ -76,7 +81,8 @@ end;
 /
 prompt --application/end_environment
 begin
-wwv_flow_api.import_end(p_auto_install_sup_obj => nvl(wwv_flow_application_install.get_auto_install_sup_obj, false));
+wwv_flow_imp.import_end(p_auto_install_sup_obj => nvl(wwv_flow_application_install.get_auto_install_sup_obj, false)
+);
 commit;
 end;
 /
